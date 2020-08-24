da74fb897b8c64c74f8144440e7703a137ecda88
Shinobi017 <badou017@gmail.com>
2020-08-24 14:18:19 +0000

    Initialisation projet

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,31 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require jquery-2.2.4.min
+//= require popper
+//= require bootstrap.min
+//= require stellar
+//= require imagesloaded.pkgd.min
+//= require isotope.pkgd.min
+//= require owl.carousel.min
+//= require jquery.ajaxchimp.min
+//= require jquery.counterup.min
+//= require jquery.waypoints.min
+//= require mail-script
+//= require contact
+//= require jquery.form
+//= require jquery.validate.min
+//= require mail-script
+//=  require theme
